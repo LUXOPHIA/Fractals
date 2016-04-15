f4310b205f5313409661ec648928e3d0f5739940
luxidea <contact@luxidea.net>
2016-04-15 10:49:47 +0900

    修正

--- ・FractalC/_LIBRARY/LIB_Complex.h
+++ ・FractalC/_LIBRARY/LIB_Complex.h
@@ -13,23 +13,23 @@
 struct TDoubleC
 {
 private:
-  ///// アクセサ
-  double GetAbs2();
-  double GetAbso();
+	///// アクセサ
+	double GetAbs2();
+	double GetAbso();
 public:
-  double R;
-  double I;
-  /////
-  TDoubleC(){};
-  TDoubleC( const double R_ );
-  TDoubleC( const double R_, const double I_ );
-  ///// プロパティ
-  __property double Abs2 = { read = GetAbs2 };
-  __property double Abso = { read = GetAbso };
-  ///// 演算子
-  friend TDoubleC operator+( const TDoubleC& A_, const TDoubleC& B_ );
-  friend TDoubleC operator*( const TDoubleC& A_, const TDoubleC& B_ );
-  ///// 型変換
+	double R;
+	double I;
+	/////
+	TDoubleC(){};
+	TDoubleC( const double R_ );
+	TDoubleC( const double R_, const double I_ );
+	///// プロパティ
+	__property double Abs2 = { read = GetAbs2 };
+	__property double Abso = { read = GetAbso };
+	///// 演算子
+	friend TDoubleC operator+( const TDoubleC& A_, const TDoubleC& B_ );
+	friend TDoubleC operator*( const TDoubleC& A_, const TDoubleC& B_ );
+	///// 型変換
 };
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TDoubleAreaC
@@ -37,20 +37,20 @@ public:
 struct TDoubleAreaC
 {
 private:
-  ///// アクセサ
-  double GetSizeR();
-  double GetSizeI();
+	///// アクセサ
+	double GetSizeR();
+	double GetSizeI();
 public:
-  TDoubleC Min;
-  TDoubleC Max;
-  /////
-  TDoubleAreaC(){};
-  TDoubleAreaC( const double MinR_, const double MinI_, const double MaxR_, const double MaxI_ );
-  ///// プロパティ
-  __property double SizeR = { read = GetSizeR };
-  __property double SizeI = { read = GetSizeI };
-  ///// 演算子
-  ///// 型変換
+	TDoubleC Min;
+	TDoubleC Max;
+	/////
+	TDoubleAreaC(){};
+	TDoubleAreaC( const double MinR_, const double MinI_, const double MaxR_, const double MaxI_ );
+	///// プロパティ
+	__property double SizeR = { read = GetSizeR };
+	__property double SizeI = { read = GetSizeI };
+	///// 演算子
+	///// 型変換
 };
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
