04648ba07b0d1290de0bb98cbc29decf8b60834e
luxidea <contact@luxidea.net>
2018-04-14 02:57:55 +0900

    Space×5 → ×4

--- FractalC/_LIBRARY/LIB.cpp
+++ FractalC/_LIBRARY/LIB.cpp
@@ -11,50 +11,50 @@
 
 float Pow2( const float X_ )
 {
-	 return X_ * X_;
+	return X_ * X_;
 }
 
 double Pow2( const double X_ )
 {
-	 return X_ * X_;
+	return X_ * X_;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 float Roo2( const float X_ )
 {
-	 return Sqrt( X_ );
+	return Sqrt( X_ );
 }
 
 double Roo2( const double X_ )
 {
-	 return Sqrt( X_ );
+	return Sqrt( X_ );
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 int Clamp( const int X_, const int Min_, const int Max_ )
 {
-	 if ( X_ < Min_ ) return Min_;
-				 else
-	 if ( X_ > Max_ ) return Max_;
-				 else return X_;
+	if ( X_ < Min_ ) return Min_;
+				else
+	if ( X_ > Max_ ) return Max_;
+				else return X_;
 }
 
 float Clamp( const float X_, const float Min_, const float Max_ )
 {
-	 if ( X_ < Min_ ) return Min_;
-				 else
-	 if ( X_ > Max_ ) return Max_;
-				 else return X_;
+	if ( X_ < Min_ ) return Min_;
+				else
+	if ( X_ > Max_ ) return Max_;
+				else return X_;
 }
 
 double Clamp( const double X_, const double Min_, const double Max_ )
 {
-	 if ( X_ < Min_ ) return Min_;
-				 else
-	 if ( X_ > Max_ ) return Max_;
-				 else return X_;
+	if ( X_ < Min_ ) return Min_;
+				else
+	if ( X_ > Max_ ) return Max_;
+				else return X_;
 }
 
 //############################################################################## ■
